18922d3d6b4bd2c8a3a2187e2b0c6a3363a4d7aa
Opus3 <admin@oticapital.co.uk>
2019-11-26 18:01:40 +0000

    Initial Commit - MultiProcessor test

---
+++ ProcessorTest/LoadSink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessorTest
+{
+    public class LoadSink
+    {
+        public static decimal GregoryLeibnizGetPI(int n)
+        {
+            decimal sum = 0;
+            decimal temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                temp = 4m / (1 + 2 * i);
+                sum += i % 2 == 0 ? temp : -temp;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ProcessorTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessorTest
+{
+    class Program
+    {
+        public const int ProcessorCount = 88;
+
+        private static IList<Task> Tasks = new List<Task>();
+        private static ILogger _logger;
+
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            _logger = serviceProvider.GetService<ILogger<Program>>();
+
+            RunLoad(ProcessorCount);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(config => config.AddConsole())
+                .Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Trace)
+                .AddTransient<Program>();
+        }
+
+        public static void RunLoad(int numberOfLoads)
+        {
+            _logger.LogDebug("Starting");
+
+            for (var slaveIdx = 0; slaveIdx < numberOfLoads; slaveIdx++)
+            {
+                Tasks.Add(GetSlaveTask(slaveIdx));
+            }
+
+            Task.WaitAll(Tasks.ToArray());
+            _logger.LogDebug("All Slaves Complete");
+        }
+
+        public static Task GetSlaveTask(int index)
+        {
+            return Task.Factory.StartNew(stateIn =>
+            {
+                var idx = (int)stateIn;
+                _logger.LogDebug($"Slave {idx}: Starting");
+                var result = LoadSink.GregoryLeibnizGetPI(int.MaxValue);
+                _logger.LogDebug($"Slave {idx}: Result={result}");
+            }, index, CancellationToken.None, TaskCreationOptions.DenyChildAttach | TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+    }
+}
